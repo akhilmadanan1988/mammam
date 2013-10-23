66366575e5063a9b4b0c38b5c37bf385a50e0041
Akhil <akhil.madanan@nlindia.com>
2013-10-23 14:10:38 +0530

    sd

---
+++ scripts/restaurants.js
@@ -0,0 +1,116 @@
+
+$(document).ready(function()
+                  {
+			
+			
+				var getdata = window.location.href.slice(window.location.href.indexOf('?')+1).split('&');  
+				
+				var zoneId = getdata[0].split('=');
+				var areaId = getdata[1].split('=');
+	
+
+  chk();
+   function chk(){
+   
+
+		actionUrl = "http://192.168.1.119:81/mammam/Development/Sites/PublicSite/json/getJson.php";
+		data = {ajaxRequest:true,method:'getRestaurantList',argumentz:'{"zoneId":"'+zoneId[1]+'","areaId":"'+areaId[1]+'","pageStart":0,"pageLimit":3}'};
+		intiateAjaxRequest("POST", actionUrl, data, res, errorInProcessing);
+	}
+	
+	
+ function intiateAjaxRequest(method, url, data, callBackFunction, erroFunction)
+		{
+		$.mobile.loading( "show", {
+			text: "Loding",
+			textVisible: true,
+			theme: "A",
+			html: ""
+		});
+			$.ajax({
+			url: url,
+			type: method, 
+			data:data,
+			success:callBackFunction,
+			error:erroFunction
+			});
+		}
+		
+	function errorInProcessing(){
+	$.mobile.loading( "hide" );
+			alert('Some errors occured. Please try again');
+			
+	}
+	
+	function res(result){
+			$.mobile.loading( "hide" );
+			var obj = (JSON.parse(result))[0].RestaurantData;
+			
+		//alert(obj[0].RestaurantName);
+		
+			
+				if(obj.length>0)
+					{
+                           
+                        for(var i=0;i<obj.length;i++)
+                        {
+                           //alert(obj[i].RestaurantId);
+                           $('#list_view').append('<li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-has-thumb ui-btn-up-c"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a href="tabArea.html?restId='+obj[i].RestaurantId+'" data-transition="slide" class="ui-link-inherit"><img src="images/OUR PARTNERS_logo_2.png" class="ui-li-thumb"><h3 class="ui-li-heading">'+obj[i].RestaurantName+'</h3><p class="ui-li-desc">Delivery Fee:<span>Free</span>  <em>Rating: <img src="images/star_1.png"> <img src="images/star_1.png"> <img src="images/star_1.png"> <img src="images/star_1.png"> <img src="images/star_2.png"></em> </p>	</a></div><span class="ui-icon ui-icon-arrow-r ui-icon-shadow">&nbsp;</span></div></li>');
+                           
+                           // i = i + 2;
+                        }  
+                    }
+
+					else
+					{
+					
+						
+					alert("Sorry no restaurants to display"); 
+					
+					}
+						
+						
+	}
+	
+	});
+	
+			
+			/*	
+                $.getJSON("http://192.168.1.119:81/mammam/Development/Sites/AdminSite/JSONService/getJson.php?method=getRestaurantList&"+getdata[0]+"&"+getdata[1]+"&pageEnd=5",function(result){
+                   
+                 
+                      var obj = result[0].RestaurantData;
+                       
+                         
+                     
+
+					if(obj.length>0)
+					{
+                           
+                        for(var i=0;i<obj.length;i++)
+                        {
+                           //alert(obj[i].RestaurantId);
+                           $('#list_view').append('<li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-has-thumb ui-btn-up-c"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a href="tabArea.html" data-transition="slide" class="ui-link-inherit"><img src="images/OUR PARTNERS_logo_2.png" class="ui-li-thumb"><h3 class="ui-li-heading">'+obj[i].RestaurantName+'</h3><p class="ui-li-desc">Delivery Fee:<span>Free</span>  <em>Rating: <img src="images/star_1.png"> <img src="images/star_1.png"> <img src="images/star_1.png"> <img src="images/star_1.png"> <img src="images/star_2.png"></em> </p>	</a></div><span class="ui-icon ui-icon-arrow-r ui-icon-shadow">&nbsp;</span></div></li>');
+                           
+                           // i = i + 2;
+                        }  
+                    }
+
+					else
+					{
+					
+						
+					alert("Sorry no restaurants to display"); 
+					
+					}
+                           
+                       }).error(function()
+                                {
+                                    
+                                   alert("Something wrong happend!!!!"); 
+                                });
+                      
+                      
+                  });
+
+			*/		
---
+++ scripts/splash.js
@@ -0,0 +1,77 @@
+
+var app = angular.module('plunker', [], function($routeProvider, $locationProvider) {
+  $routeProvider.otherwise({templateUrl: 'index.html'});
+});
+
+
+app.directive('jqueryMobileTpl', function() {
+  return {
+    link: function(scope, elm, attr) {
+      elm.trigger('create');
+    }
+  };
+});
+
+
+	var devices;
+	document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+    //
+    function onDeviceReady()
+	{
+		
+        checkConnection();
+		
+    }
+
+        function checkConnection()
+		{
+            var networkState = navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'WiFi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.CELL]     = 'Cell generic connection';
+            states[Connection.NONE]     = 'No network connection';
+
+           
+			
+			
+			if(states[networkState] == 'No network connection')
+			{
+			
+			window.alert("No network connection.Connect to network and relaunch Mammam");
+			
+			 navigator.app.exitApp();
+			 
+			}
+			else
+			{
+				deviceinfo();		
+			}
+			
+			
+        }
+		
+		function deviceinfo()
+		{
+				var dev = device.platform;	
+					alert(dev); 
+		
+		}
+		
+		
+		
+	$(document).ready(function(){
+                  
+				  
+		
+
+		});
+		
+		
