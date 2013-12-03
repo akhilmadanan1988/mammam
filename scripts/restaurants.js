d77a4fae596e87e80730f3bad455f850a6bab321
Akhil <akhil.madanan@nlindia.com>
2013-12-03 20:02:10 +0530

    asd
    as

---
+++ scripts/restaurants.js
@@ -0,0 +1,127 @@
+	
+				var getdata = window.location.href.slice(window.location.href.indexOf('?')+1).split('&');  
+				
+				var zoneId = getdata[0].split('=');
+				var areaId = getdata[1].split('=');
+				var restRating;
+				var restObj;
+				var restList;
+				var restUrl;
+				var loadMore = 6;
+			getData(3);
+  
+   function getData(pageLimit)
+	{
+		restUrl = rootPath;
+		data =	{ajaxRequest:true,method:'getRestaurantList',argumentz:'{"zoneId":"'+zoneId[1]+'","areaId":"'+areaId[1]+'","pageStart":0,"pageLimit":'+pageLimit+'}'};
+		intiateAjaxRequest("POST", restUrl, data, restResponse, restErrorInProcessing);
+	}
+	
+		
+	function restErrorInProcessing(response, status, xhr)
+	{
+			$.mobile.loading( "hide" );
+			alert(status+'Some errors occured. Please try again');
+			
+	}
+	
+	function restResponse(result, textStatus , xhr)
+	{
+	
+		
+			$.mobile.loading( "hide" );
+		
+		
+		if(typeof result=="object")
+			{
+		
+			//var menuObj = JSON.parse(result);
+			restList(result);
+				
+				
+			}
+			
+			else
+			{
+				//alert(JSON.parse(result));
+				
+					restList(JSON.parse(result));
+			}
+				
+		
+			
+						
+		}
+
+
+	function restList(result)
+		{
+			
+			var restList = (result)[0].RestaurantData;
+			
+				//alert(restList[0].RestaurantId)	
+				if(restList.length>0)
+					{
+                      $('#list_view').html('');     
+					
+						for(var i=0;i<restList.length;i++)
+						{
+													
+						//alert((obj[i].RestaurantLogo));
+						
+						
+                           $('#list_view').append('<li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-has-thumb ui-btn-up-c"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a href="menuItem.html?searchId=0&restId='+restList[i].RestaurantId+'&restName='+restList[i].RestaurantName+'" data-transition="slide" class="ui-link-inherit"><img src="'+imgURL+restList[i].RestaurantLogo+'"  onerror=this.src="images/placeholder.png"; class="ui-li-thumb"><h3 class="ui-li-heading">'+restList[i].RestaurantName+'</h3><p class="ui-li-desc">Delivery Fee:<span>Free</span>  <em id="ratingStar'+restList[i].RestaurantId+'">Rating:  </em></p>	</a></div><span class="ui-icon ui-icon-arrow-r ui-icon-shadow">&nbsp;</span></div></li>');
+                           
+						   if(restList[i].RattingValue == 0)
+						   {
+						     $('#ratingStar'+restList[i].RestaurantId).append('<img src="images/star_2.png"> <img src="images/star_2.png"><img src="images/star_2.png"><img src="images/star_2.png"><img src="images/star_2.png">');
+							}
+						   if(restList[i].RattingValue == 1)
+						   {
+						    $('#ratingStar'+restList[i].RestaurantId).append('<img src="images/star_1.png"> <img src="images/star_2.png"><img src="images/star_2.png"><img src="images/star_2.png"><img src="images/star_2.png">');
+							}
+							 if(restList[i].RattingValue == 2)
+						   {
+						    $('#ratingStar'+restList[i].RestaurantId).append('<img src="images/star_1.png"> <img src="images/star_1.png"><img src="images/star_2.png"><img src="images/star_2.png"><img src="images/star_2.png">');
+							}
+							
+							 if(restList[i].RattingValue == 3)
+						   {
+						    $('#ratingStar'+restList[i].RestaurantId).append('<img src="images/star_1.png"> <img src="images/star_1.png"><img src="images/star_1.png"><img src="images/star_2.png"><img src="images/star_2.png">');
+							}
+							
+							 if(restList[i].RattingValue == 4)
+						   {
+						     $('#ratingStar'+restList[i].RestaurantId).append('<img src="images/star_1.png"> <img src="images/star_1.png"><img src="images/star_1.png"><img src="images/star_1.png"><img src="images/star_2.png">');
+							}
+							
+							 if(restList[i].RattingValue == 5)
+						   {
+						     $('#ratingStar'+restList[i].RestaurantId).append('<img src="images/star_1.png"> <img src="images/star_1.png"><img src="images/star_1.png"><img src="images/star_1.png"><img src="images/star_1.png">');
+							}
+                           // i = i + 2;
+							
+						
+						
+						}	
+						
+						$('#list_view').append('<a data-role="button" onclick="getData('+loadMore+')" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-theme="c" class="ui-btn ui-shadow ui-btn-corner-all ui-btn-up-c"><span class="ui-btn-inner"><span class="ui-btn-text">             Load More         </span></span></a>');
+						
+					loadMore = loadMore + 3;
+						
+                    }
+
+					else
+					{
+											
+					alert("Sorry no restaurants to display"); 
+					
+					}
+						
+			
+		}
+	
+	
+	
+	
+		
---
+++ scripts/signIn.js
@@ -0,0 +1,183 @@
+var signInUserId;
+var signInUserName;
+var signInUserPassword;
+var signInUserFirstName;
+function signIn()
+{
+    
+            userPas = document.getElementById("signPass").value;
+			userName = document.getElementById("signinEmail").value;
+            
+    
+    if(userName==""||userPas=="")
+		{
+            alert("Please fill all the fields.");
+                    
+        }
+    
+    else
+    {
+      checkEmail();
+        
+    }
+    
+    
+}
+
+
+function checkEmail() {
+
+     signInUserName = document.getElementById('signinEmail').value;
+     signInUserPassword = document.getElementById("signPass").value;
+    
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!filter.test(signInUserName)) 
+	{
+	
+    alert('Please provide a valid email address');
+   signInUserName.focus;
+    return false;
+	
+	}
+	
+	else
+	{
+        
+		actionUrl = rootPath;
+        data = {ajaxRequest:true,method:'validateCustomerLogin',argumentz:'{"userName":"'+signInUserName+'","password":"'+signInUserPassword+'"}'};
+			intiateAjaxRequest("POST", actionUrl, data, signInResp, errorInSignIn);
+        
+	return true;
+	}
+ }
+
+function signInResp(response)
+{
+   
+    $.mobile.loading( "hide" );
+    
+//    alert(JSON.stringify(response, null, 2));
+    
+    var signInRes = JSON.parse(JSON.stringify(response, null, 2));
+    
+     
+//     alert(signInRes.status);
+    
+    if(String(signInRes.status) == "success")
+    {
+        
+        alert(signInRes.message);
+        signInUserId = signInRes.intCustomerId;
+        signInUserFirstName = signInRes.strFirstName;
+        
+        var db = window.openDatabase("dbmammam", "1.0", "mammam", 1000000);
+		db.transaction(populateDBSignIn, errorSignIn, successSignIn);
+        
+    }
+    
+    if(String(signInRes.status) == "faild")
+    {
+        
+         alert(signInRes.message);
+        
+    }
+    
+}
+
+
+function populateDBSignIn(tx)
+{
+    
+    
+     tx.executeSql('CREATE TABLE IF NOT EXISTS settings (custId unique, userName, email unique,phNo,password,isActive)');
+		tx.executeSql('DELETE FROM settings');
+		 tx.executeSql('INSERT INTO settings (custId, userName, email,phNo,password,isActive) VALUES("'+signInUserId+'","'+signInUserFirstName+'","'+signInUserName+'","","'+signInUserPassword+'","Y")');
+    
+}
+
+
+function errorSignIn(tx, err)
+{
+    
+    
+    alert("some error occured");
+    
+}
+
+function successSignIn(tblCart,result)
+{
+      
+    document.location.href = 'index.html';
+    
+}
+
+
+function errorInSignIn(response, status, xhr)
+{
+    
+    $.mobile.loading( "hide" );
+    
+    alert("Sorry some error occured");
+  
+    
+}
+
+function forgetPwd()
+{
+    
+    var frg_pwd = document.getElementById("frgEmail").value;
+    
+   var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!filter.test(frg_pwd)) 
+	{
+	
+    alert('Please provide a valid email address');
+        
+        
+    }
+    
+    
+    else
+    {
+        
+        actionUrl = rootPath;
+        data = {ajaxRequest:true,method:'forgotPasswordCustomer',argumentz:'{"option":"checkExists","email":"'+frg_pwd+'","code":0}'};
+			intiateAjaxRequest("POST", actionUrl, data, frgInResp, errorInSignIn);
+        
+    }
+    
+    
+    
+}
+
+
+function frgInResp(response)
+{
+    
+    $.mobile.loading( "hide" );
+    
+//    alert(JSON.stringify(response, null, 2));
+    
+    var frgPwdnRes = JSON.parse(JSON.stringify(response, null, 2));
+    
+    
+    
+    if(frgPwdnRes.status == 1)
+    {
+        
+        alert("Please check your email");
+        
+        $( "#popupForPwd" ).popup( "close" )
+        
+    }
+    else if(frgPwdnRes.status == 0)
+    {
+        
+        alert("email not found");
+    }
+    
+    
+}
+
