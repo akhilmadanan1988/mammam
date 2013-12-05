73bc93eac1eccd1ee0f72851a655bc0e4a3d6233
Akhil <akhil.madanan@nlindia.com>
2013-12-05 10:31:52 +0530

    asd
    asd

--- scripts/appVariable.js
+++
@@ -1,4 +0,0 @@
-
-//	var rootPath = "http://192.168.1.119:81/mammam/Development/Sites/PublicSite/JSONService/jsonApi.php";
-var rootPath = "http://mammam.biztro.in/JSONService/jsonApi.php";
-var imgURL = "http://mammam.biztro.in/AdminSite/resources/uploads/advertisement/HORZ_01/";
